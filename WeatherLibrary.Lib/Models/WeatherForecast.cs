5dc415f8febaafcd46dc1c02a65fb6f9be1300ac
Tom Overton <88758960+tomboverton@users.noreply.github.com>
2023-04-15 22:36:49 -0500

    first checkin

---
+++ WeatherLibrary.Lib/Models/WeatherForecast.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherLibrary.Lib.Models
+{
+    public class WeatherForecast
+    {
+        public string City { get; set; }
+        public string Temperature { get; set; }
+        public string Conditions { get; set; }
+    }
+}
---
+++ WeatherLibrary.Lib/WeatherReporter.cs
@@ -0,0 +1,33 @@
+﻿using WeatherLibrary.Lib.Models;
+namespace WeatherLibrary.Lib
+{
+    public class WeatherReporter
+    {
+       public WeatherForecast GetForecast(string _City)
+        {
+            WeatherForecast forecast = new WeatherForecast();
+
+            switch (_City)
+                { 
+               case "Plano":
+                    forecast.City = _City;
+                    forecast.Temperature = "81f";
+                    forecast.Conditions = "Cloudy";
+                break;
+                case "Los Angeles":
+                    forecast.City = _City;
+                    forecast.Temperature = "72f";
+                    forecast.Conditions = "Sunny";
+                    break;
+                default:
+                    forecast.City = "N/A";
+                    forecast.Temperature = "0f";
+                    forecast.Conditions = "N/A";
+                    break;
+            }
+
+            return forecast;
+
+        }
+    }
+}
